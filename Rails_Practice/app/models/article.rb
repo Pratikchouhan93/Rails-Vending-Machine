8894eebb478131b68a549f985da152899ddaa6c0
pratik <pratikc@shriffle.com>
2022-09-15 19:44:15 +0530

    elastic search

--- Rails_Practice/app/models/article.rb
+++ Rails_Practice/app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
   has_many :comments
   
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
+
+  index_name([Rails.env,base_class.to_s.pluralize.underscore].join('_'))
+  # index_name Rails.application.class.parent_name.underscore
+  # document_type self.name.downcase
 end
